0007f12caae01d9d7ec81308556b9c0c73df50d5
Dany BF <daniela.dbf95@gmail.com>
2024-01-19 00:33:41 -0300

    first day

--- pokedex-react/src/App.jsx
+++ pokedex-react/src/App.jsx
@@ -6,8 +6,32 @@ function App() {
   return (
     <>
       <h1>POKEMON + REACT</h1>
+
+      <navbar>Barra de busqueda </navbar>
+      <button>search</button>
       <div class='pokemons'>
+        <p>#{pokemons.id}</p>
         <p>{pokemons.name}</p>
+        <img src={pokemons.imageUrl} class='miniatura'></img>
+        <img src={pokemons.imgType} class='imgtype'></img>
+      </div>
+      <div class='pokemons'>
+        <p>#{pokemons.id}</p>
+        <p>{pokemons.name}</p>
+        <img src={pokemons.imageUrl} class='miniatura'></img>
+        <img src={pokemons.imgType} class='imgtype'></img>
+      </div>
+      <div class='pokemons'>
+        <p>#{pokemons.id}</p>
+        <p>{pokemons.name}</p>
+        <img src={pokemons.imageUrl} class='miniatura'></img>
+        <img src={pokemons.imgType} class='imgtype'></img>
+      </div>
+      <div class='pokemons'>
+        <p>#{pokemons.id}</p>
+        <p>{pokemons.name}</p>
+        <img src={pokemons.imageUrl} class='miniatura'></img>
+        <img src={pokemons.imgType} class='imgtype'></img>
       </div>
     </>
   )
--- pokedex-react/src/components/pokemons.jsx
+++ pokedex-react/src/components/pokemons.jsx
@@ -1,4 +1,8 @@
 export const pokemons = {
+  id: '1',
   name: 'Pikachu',
-  pic: 'hello',
+  imageUrl:
+    'https://raw.githubusercontent.com/DanyBF/pokedexReact/main/pokedex-react/src/assets/pikachu.png',
+  imgType:
+    'https://github.com/DanyBF/pokedexReact/blob/main/pokedex-react/src/assets/electric.png?raw=true',
 }
